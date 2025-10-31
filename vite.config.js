f876b486e35b17cf6d7b96200f9fb10fde672c1c
Carlos Carranza <carforck@gmail.com>
2025-10-31 18:44:19 -0500

    Preparacion Despliegue

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ asegura que las rutas de assets sean relativas
+  build: {
+    outDir: 'dist', // ✅ carpeta que Vercel usará como salida
+    assetsDir: 'assets', // (opcional) organiza los archivos estáticos
+  },
+  server: {
+    port: 5173, // (opcional) útil si haces pruebas locales
+    open: true, // abre automáticamente el navegador
+  },
 })
