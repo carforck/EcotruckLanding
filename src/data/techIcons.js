a8817ba7b0833eabecbb54d976ceb98727a308ac
Carlos Carranza <carforck@gmail.com>
2025-10-31 19:03:14 -0500

    Fix: rutas SVG en techIcons

--- src/data/techIcons.js
+++ src/data/techIcons.js
@@ -1,148 +1,63 @@
 // src/data/techIcons.js
+
+import apache from '../assets/icons/Apache.svg'
+import azure from '../assets/icons/azure.svg'
+import bash from '../assets/icons/bash.svg'
+import canva from '../assets/icons/canva.svg'
+import chrome from '../assets/icons/chrome.svg'
+import css3 from '../assets/icons/css3.svg'
+import git from '../assets/icons/git.svg'
+import githubActions from '../assets/icons/github actions.svg'
+import githubCodespaces from '../assets/icons/github codespaces.svg'
+import github from '../assets/icons/github.svg'
+import googleCloud from '../assets/icons/google cloud.svg'
+import html5 from '../assets/icons/html5.svg'
+import java from '../assets/icons/java.svg'
+import javascript from '../assets/icons/javascript.svg'
+import jira from '../assets/icons/jira.svg'
+import json from '../assets/icons/json.svg'
+import materialUI from '../assets/icons/material ui.svg'
+import mongodb from '../assets/icons/mongodb.svg'
+import nodejs from '../assets/icons/node.js.svg'
+import npm from '../assets/icons/npm.svg'
+import postman from '../assets/icons/postman.svg'
+import powershell from '../assets/icons/powershell.svg'
+import react from '../assets/icons/react (1).svg'
+import sass from '../assets/icons/sass.svg'
+import spring from '../assets/icons/spring.svg'
+import tailwind from '../assets/icons/tailwind CSS.svg'
+import typescript from '../assets/icons/typescript.svg'
+import vscode from '../assets/icons/VS CODE.svg'
+import vite from '../assets/icons/vite.js.svg'
+
 export const techIcons = [
-  {
-    name: "Apache",
-    icon: "/src/assets/icons/Apache.svg",
-    url: "https://apache.org/",
-  },
-  {
-    name: "Azure",
-    icon: "/src/assets/icons/azure.svg",
-    url: "https://azure.microsoft.com/",
-  },
-  {
-    name: "Bash",
-    icon: "/src/assets/icons/bash.svg",
-    url: "https://www.gnu.org/software/bash/",
-  },
-  {
-    name: "Canva",
-    icon: "/src/assets/icons/canva.svg",
-    url: "https://www.canva.com/",
-  },
-  {
-    name: "Chrome",
-    icon: "/src/assets/icons/chrome.svg",
-    url: "https://www.google.com/chrome/",
-  },
-  {
-    name: "CSS3",
-    icon: "/src/assets/icons/css3.svg",
-    url: "https://www.w3.org/Style/CSS/",
-  },
-  {
-    name: "Git",
-    icon: "/src/assets/icons/git.svg",
-    url: "https://git-scm.com/",
-  },
-  {
-    name: "GitHub Actions",
-    icon: "/src/assets/icons/github actions.svg",
-    url: "https://github.com/features/actions",
-  },
-  {
-    name: "GitHub CodeSpaces",
-    icon: "/src/assets/icons/github codespaces.svg",
-    url: "https://github.com/features/codespaces",
-  },
-  {
-    name: "GitHub",
-    icon: "/src/assets/icons/github.svg",
-    url: "https://github.com/",
-  },
-  {
-    name: "Google Cloud",
-    icon: "/src/assets/icons/google cloud.svg",
-    url: "https://cloud.google.com/",
-  },
-  {
-    name: "HTML5",
-    icon: "/src/assets/icons/html5.svg",
-    url: "https://www.w3.org/TR/html52/",
-  },
-  {
-    name: "Java",
-    icon: "/src/assets/icons/java.svg",
-    url: "https://www.java.com/",
-  },
-  {
-    name: "JavaScript",
-    icon: "/src/assets/icons/javascript.svg",
-    url: "https://www.javascript.com/",
-  },
-  {
-    name: "Jira",
-    icon: "/src/assets/icons/jira.svg",
-    url: "https://www.atlassian.com/software/jira",
-  },
-  {
-    name: "JSON",
-    icon: "/src/assets/icons/json.svg",
-    url: "https://www.json.org/",
-  },
-  {
-    name: "Material UI",
-    icon: "/src/assets/icons/material ui.svg",
-    url: "https://mui.com/",
-  },
-  {
-    name: "MongoDB",
-    icon: "/src/assets/icons/mongodb.svg",
-    url: "https://www.mongodb.com/",
-  },
-  {
-    name: "Node.js",
-    icon: "/src/assets/icons/node.js.svg",
-    url: "https://nodejs.org/",
-  },
-  {
-    name: "NPM",
-    icon: "/src/assets/icons/npm.svg",
-    url: "https://www.npmjs.com/",
-  },
-  {
-    name: "Postman",
-    icon: "/src/assets/icons/postman.svg",
-    url: "https://www.postman.com/",
-  },
-  {
-    name: "PowerShell",
-    icon: "/src/assets/icons/powershell.svg",
-    url: "https://docs.microsoft.com/en-us/powershell/",
-  },
-  {
-    name: "React",
-    icon: "/src/assets/icons/react (1).svg",
-    url: "https://reactjs.org/",
-  },
-  {
-    name: "sass",
-    icon: "/src/assets/icons/sass.svg",
-    url: "https://sass-lang.com/",
-  },
-  {
-    name: "Spring Boot",
-    icon: "/src/assets/icons/spring.svg",
-    url: "https://spring.io/projects/spring-boot",
-  },
-  {
-    name: "Tailwind CSS",
-    icon: "/src/assets/icons/tailwind CSS.svg",
-    url: "https://tailwindcss.com/",
-  },
-  {
-    name: "TypeScript",
-    icon: "/src/assets/icons/typescript.svg",
-    url: "https://www.typescriptlang.org/",
-  },
-  {
-    name: "VS Code",
-    icon: "/src/assets/icons/VS CODE.svg",
-    url: "https://code.visualstudio.com/",
-  },
-  {
-    name: "Vite",
-    icon: "/src/assets/icons/vite.js.svg",
-    url: "https://vitejs.dev/",
-  },
-];
+  { name: "Apache", icon: apache, url: "https://apache.org/" },
+  { name: "Azure", icon: azure, url: "https://azure.microsoft.com/" },
+  { name: "Bash", icon: bash, url: "https://www.gnu.org/software/bash/" },
+  { name: "Canva", icon: canva, url: "https://www.canva.com/" },
+  { name: "Chrome", icon: chrome, url: "https://www.google.com/chrome/" },
+  { name: "CSS3", icon: css3, url: "https://www.w3.org/Style/CSS/" },
+  { name: "Git", icon: git, url: "https://git-scm.com/" },
+  { name: "GitHub Actions", icon: githubActions, url: "https://github.com/features/actions" },
+  { name: "GitHub CodeSpaces", icon: githubCodespaces, url: "https://github.com/features/codespaces" },
+  { name: "GitHub", icon: github, url: "https://github.com/" },
+  { name: "Google Cloud", icon: googleCloud, url: "https://cloud.google.com/" },
+  { name: "HTML5", icon: html5, url: "https://www.w3.org/TR/html52/" },
+  { name: "Java", icon: java, url: "https://www.java.com/" },
+  { name: "JavaScript", icon: javascript, url: "https://www.javascript.com/" },
+  { name: "Jira", icon: jira, url: "https://www.atlassian.com/software/jira" },
+  { name: "JSON", icon: json, url: "https://www.json.org/" },
+  { name: "Material UI", icon: materialUI, url: "https://mui.com/" },
+  { name: "MongoDB", icon: mongodb, url: "https://www.mongodb.com/" },
+  { name: "Node.js", icon: nodejs, url: "https://nodejs.org/" },
+  { name: "NPM", icon: npm, url: "https://www.npmjs.com/" },
+  { name: "Postman", icon: postman, url: "https://www.postman.com/" },
+  { name: "PowerShell", icon: powershell, url: "https://docs.microsoft.com/en-us/powershell/" },
+  { name: "React", icon: react, url: "https://reactjs.org/" },
+  { name: "sass", icon: sass, url: "https://sass-lang.com/" },
+  { name: "Spring Boot", icon: spring, url: "https://spring.io/projects/spring-boot" },
+  { name: "Tailwind CSS", icon: tailwind, url: "https://tailwindcss.com/" },
+  { name: "TypeScript", icon: typescript, url: "https://www.typescriptlang.org/" },
+  { name: "VS Code", icon: vscode, url: "https://code.visualstudio.com/" },
+  { name: "Vite", icon: vite, url: "https://vitejs.dev/" },
+]
